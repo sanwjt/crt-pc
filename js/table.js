30641e4c6abc8b585d5de6fe16944bc923fe5db0
1532175728@qq.com <1532175728@qq.com>
2020-11-20 18:47:54 +0800

    wojwojweojewjeowjeowjowe

--- js/table.js
+++ js/table.js
@@ -10,6 +10,7 @@ function zk(){
 	var join_zp_c = document.querySelectorAll(".join_zp_c");
 	var join_zp_n = document.querySelectorAll(".join_zp_n");
 	var join_zp_length = join_zp_c.length;
+	// var orange_boder =
 	for(var i=0;i<join_zp_length;i++){
 		join_zp_c[i].index = i;
 		join_zp_c[i].onclick = function(){
@@ -17,16 +18,21 @@ function zk(){
 			if(isf == 'join_zp_n join_active'){
 				join_zp_n[this.index].className = 'join_zp_n';
 				$('.detail_btn').eq(this.index).html('详情');
-				$('.detail_btn').eq(this.index).css({color:"#FF8721"})
+				$('.detail_btn').eq(this.index).css({color:"#FF8721"});
+				
+				
 			}else{
 				for(var i=0;i<join_zp_length;i++){
 					join_zp_n[i].className = 'join_zp_n';
 					$('.detail_btn').eq(this.index).html('详情');
 					$('.detail_btn').eq(this.index).css({color:"#FF8721"});
+				
+
 				}
 				join_zp_n[this.index].className = 'join_zp_n join_active';
 				$('.detail_btn').eq(this.index).html('收起');
 				$('.detail_btn').eq(this.index).css({color:"#7EDCFF"});
+
 			}
 		}
 	}
